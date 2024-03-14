67b2bb655e83595b38bd8d11dae2fb7ddd25c5e0
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-14 19:26:28 +0100

    Reorganize login and register templates

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -36,7 +36,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('auth/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
