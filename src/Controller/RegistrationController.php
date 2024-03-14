b34eb43c75ea11495ea4699aa65d666b41c1288f
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-14 20:12:57 +0100

    Add initial version of the homepage

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('auth/register.html.twig', [
