addfc7f23b2519a634823c2a6372dc8f5555bb89
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-16 19:38:47 +0100

    Revert "Install doctrine extensions bundle"
    This reverts commit f6a5a0e5006649ca0fd058e15471e3240764a186.

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
