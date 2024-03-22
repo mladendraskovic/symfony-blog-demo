0bb096382b13d4745cf2ba025b93356cdb330e0a
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-22 12:01:17 +0100

    Implement profile update page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,26 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\Comment;
-use App\Form\CommentType;
-use App\Repository\PostRepository;
+use App\Entity\User;
+use App\Form\ProfileType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ProfileController extends AbstractController
 {
     private $userRepository;
+    private $passwordHasher;
     private $translator;
 
-    public function __construct(UserRepository $userRepository, TranslatorInterface $translator)
+    public function __construct(UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, TranslatorInterface $translator)
     {
         $this->userRepository = $userRepository;
+        $this->passwordHasher = $passwordHasher;
         $this->translator = $translator;
     }
 
@@ -40,46 +41,34 @@ class ProfileController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/profile/edit", name="app_profile", methods={"GET"}))
-//     */
-//    public function edit(Request $request): Response
-//    {
-//        $locale = $request->getLocale();
-//        $userId = $this->getUser() ? $this->getUser()->getId() : null;
-//
-//        $post = $this->postRepository->findPostBySlug($slug, $locale, $userId);
-//        $likesCount = $this->postRepository->getLikesCount($post->getId());
-//
-//        if (!$post) {
-//            throw $this->createNotFoundException('No post found for slug ' . $slug);
-//        }
-//
-//        $comment = new Comment();
-//
-//        $form = $this->createForm(CommentType::class, $comment);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            if (!$this->getUser()) {
-//                throw $this->createAccessDeniedException('You must be logged in to comment.');
-//            }
-//
-//            $comment->setPost($post);
-//            $comment->setAuthor($this->getUser());
-//
-//            $this->entityManager->persist($comment);
-//            $this->entityManager->flush();
-//
-//            $this->addFlash('success', $this->translator->trans('Your comment was saved successfully.'));
-//
-//            return $this->redirectToRoute('app_post', ['slug' => $slug]);
-//        }
-//
-//        return $this->render('pages/post.html.twig', [
-//            'post' => $post,
-//            'likesCount' => $likesCount,
-//            'form' => $form->createView()
-//        ]);
-//    }
+    /**
+     * @Route("/profile/edit", name="app_profile_edit", methods={"GET", "POST"}))
+     * @IsGranted("IS_AUTHENTICATED")
+     */
+    public function edit(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($password = $form->get('password')->getData()) {
+                $user->setPassword(
+                    $this->passwordHasher->hashPassword($user, $password)
+                );
+            }
+
+            $this->userRepository->add($user, true);
+
+            $this->addFlash('success', $this->translator->trans('Profile updated successfully.'));
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('pages/profile-edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
