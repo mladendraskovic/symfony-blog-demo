6b30a0bda27df5c68f9974d4a0256ec90e086d3e
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-22 10:38:05 +0100

    Cleanup repository classes

--- src/Repository/PostTranslationRepository.php
+++ src/Repository/PostTranslationRepository.php
@@ -38,29 +38,4 @@ class PostTranslationRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return PostTranslation[] Returns an array of PostTranslation objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?PostTranslation
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
