209e4a8cf7e35caf57b4823b3becb286a2efeb3f
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-22 09:56:02 +0100

    Update like controller

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -5,25 +5,19 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Repository\PostRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LikeController extends AbstractController
 {
     private $postRepository;
-    private $entityManager;
-    private $translator;
 
-    public function __construct(PostRepository $postRepository, EntityManagerInterface $entityManager, TranslatorInterface $translator)
+    public function __construct(PostRepository $postRepository)
     {
         $this->postRepository = $postRepository;
-        $this->entityManager = $entityManager;
-        $this->translator = $translator;
     }
 
     /**
@@ -35,7 +29,9 @@ class LikeController extends AbstractController
         /** @var User $user */
         $user = $this->getUser();
 
-        $this->postRepository->likePost($post->getId(), $user->getId());
+        if ($this->isCsrfTokenValid('like' . $post->getId(), $request->request->get('_token'))) {
+            $this->postRepository->likePost($post->getId(), $user->getId());
+        }
 
         return $this->redirect($request->headers->get('referer') ?: $this->generateUrl('app_home'));
     }
@@ -49,7 +45,9 @@ class LikeController extends AbstractController
         /** @var User $user */
         $user = $this->getUser();
 
-        $this->postRepository->unlikePost($post->getId(), $user->getId());
+        if ($this->isCsrfTokenValid('unlike' . $post->getId(), $request->request->get('_token'))) {
+            $this->postRepository->unlikePost($post->getId(), $user->getId());
+        }
 
         return $this->redirect($request->headers->get('referer') ?: $this->generateUrl('app_home'));
     }
