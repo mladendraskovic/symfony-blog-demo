887266a2aaa83b542bbe74a50930f4f8911414b2
Mladen Draskovic <draskovicmladen@gmail.com>
2024-03-26 20:41:27 +0100

    Add validation for registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -27,9 +28,23 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('name', null, [
                 'label' => $this->translator->trans('Name'),
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                    ]),
+                ],
             ])
             ->add('email', null, [
                 'label' => $this->translator->trans('Email'),
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                    new Length([
+                        'max' => 180,
+                    ]),
+                ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
